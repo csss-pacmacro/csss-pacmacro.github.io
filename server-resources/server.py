bd2d4e8213b0683b746adc3864597a1774cedbff
Gabe <24978329+EarthenSky@users.noreply.github.com>
2022-06-28 17:45:53 -0700

    Update server.py

--- server-resources/server.py
+++ server-resources/server.py
@@ -41,7 +41,7 @@ class CORSHandler(BaseHTTPRequestHandler):
         #arg2 = split_path[1] if len(split_path) > 2 else None
 
         # TODO: don't send password in plaintext
-        if path_head == "/host" & arg1 == thepassword:
+        if path_head == "/host" and arg1 == thepassword:
             self.wfile.write("\npassword is correct, woo!".encode('utf-8'))
             self.wfile.write("maps:".encode('utf-8'))
             self.wfile.write("name: the cool map".encode('utf-8'))
@@ -49,7 +49,7 @@ class CORSHandler(BaseHTTPRequestHandler):
             self.wfile.write("edges: i,j i,j i,j i,j ".encode('utf-8'))
 
             # TODO: load maps from disk
-        elif path_head == "/host" & arg1 != thepassword:
+        elif path_head == "/host" and arg1 != thepassword:
             self.wfile.write("\nwrong password T-T".encode('utf-8'))
 
 
@@ -73,7 +73,7 @@ def run(server_class=HTTPServer, handler_class=CORSHandler, port=8080):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd on port' + str(port) + '...\n')
+    logging.info('Starting httpd on port ' + str(port) + '...\n')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
