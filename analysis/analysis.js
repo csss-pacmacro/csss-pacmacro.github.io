28ef3b6897251c7c434e4964653e11739aaf9520
Gabe <24978329+EarthenSky@users.noreply.github.com>
2023-08-22 10:23:09 -0700

    fix style and start behaviour

--- analysis/analysis.js
+++ analysis/analysis.js
@@ -1,14 +1,39 @@
-
 var isActive = false;
-function toggleAnalysis() {
-	if (isActive) {
-		document.getElementById("button").style.backgroundColor = "green";
-	} else {
-		document.getElementById("button").style.backgroundColor = "orange";
-	}
-	isActive = !isActive;
-	console.log("should start? " + isActive);
-
-
+function onStart() {
+	document.getElementById("button").style.backgroundColor = "#c6f0a1";
+	document.getElementById("p").innerHTML = "paused";
 }
 
+var fid;
+function toggleAnalysis() {
+	if (isActive) {
+		document.getElementById("button").style.backgroundColor = "#c6f0a1";
+		document.getElementById("p").innerHTML = "paused";
+	} else {
+		document.getElementById("button").style.backgroundColor = "#f0cca1";
+		document.getElementById("p").innerHTML = "running";
+	}
+
+	isActive = !isActive;
+	console.log("should start? " + isActive);
+	
+	if (isActive) { 
+		fid = setInterval(() => {
+			let ping = pingGoogle();
+			let data = locationData();
+			console.log("ping: " + ping);
+			
+			// TODO: process & save this
+		}, 1000);
+	} else {
+		clearInterval(fid);
+	}
+}
+
+function pingGoogle() {
+	// TODO: ping google
+}
+
+function locationData() {
+	// TODO: get location data
+}
